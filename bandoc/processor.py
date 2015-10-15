1fdc9e599b5070989ab6fa28b0e26d1eec059a37
Michał Matczuk <michal.matczuk@7bulls.com>
2015-10-15 22:17:55 +0200

    * Processor refactoring

--- bandoc/processor.py
+++ bandoc/processor.py
@@ -8,6 +8,9 @@ __author__ = 'mmatczuk'
 class Processor(object):
     @staticmethod
     def get_max_region(samples):
+        """
+        See :_get_region:.
+        """
         m0, m1 = MATRIX_SIZE, 0
         for s in samples:
             c0, c1 = Processor._get_region(s)
@@ -16,7 +19,7 @@ class Processor(object):
             if c1 > m1:
                 m1 = c1
 
-        return (m0, m1)
+        return m0, m1
 
     @staticmethod
     def _get_region(sample):
@@ -42,7 +45,7 @@ class Processor(object):
 
         assert a < b, '%s, %s\n%s' % (a, b, sample)
 
-        return (a, b)
+        return a, b
 
     @staticmethod
     def clip_to_region(region, samples):
@@ -63,33 +66,44 @@ class Processor(object):
 
     @staticmethod
     def split_avg(samples):
-        joined_sample = Sample()
-        del joined_sample.values[0:MATRIX_SIZE - len(samples[0].values)]
+        merged = Processor._merge(samples)
+
+        l = len(merged.values)
+        l2 = l / 2  # number of sampling rows halved
+        cell_count = l2 * 4 * len(samples)
+        return {
+            'q1': Processor._sum(merged, l,  l2, -2,  2) / cell_count,
+            'q2': Processor._sum(merged, l,  l2,  2,  6) / cell_count,
+            'q3': Processor._sum(merged, l,  l2,  6, 10) / cell_count,
+            'q4': Processor._sum(merged, l,  l2, 10, 14) / cell_count,
+            'q5': Processor._sum(merged, l2, 0,  -2,  2) / cell_count,
+            'q6': Processor._sum(merged, l2, 0,   2,  6) / cell_count,
+            'q7': Processor._sum(merged, l2, 0,   6, 10) / cell_count,
+            'q8': Processor._sum(merged, l2, 0,  10, 14) / cell_count,
+
+            'prm': Processor._sum(merged, l, l2,  2, 14) / (3 * cell_count),
+        }
+
+    @staticmethod
+    def _merge(samples):
+        result = Sample()
+        del result.values[0:MATRIX_SIZE - len(samples[0].values)]
 
         for s in samples:
             for i in range(len(s.values)):
                 for j in range(MATRIX_SIZE):
-                    joined_sample.values[i][j] += s.values[i][j]
-
-        multi = len(samples)
-
-        l = len(joined_sample.values)
-        count = (l / 2) * 4
-        return {
-            'q1': Processor._sum(joined_sample, l, l / 2, -2, 2) / (count * multi),
-            'q2': Processor._sum(joined_sample, l, l / 2, 2, 6) / (count * multi),
-            'q3': Processor._sum(joined_sample, l, l / 2, 6, 10) / (count * multi),
-            'q4': Processor._sum(joined_sample, l, l / 2, 10, 14) / (count * multi),
-            'q5': Processor._sum(joined_sample, l / 2, 0, -2, 2) / (count * multi),
-            'q6': Processor._sum(joined_sample, l / 2, 0, 2, 6) / (count * multi),
-            'q7': Processor._sum(joined_sample, l / 2, 0, 6, 10) / (count * multi),
-            'q8': Processor._sum(joined_sample, l / 2, 0, 10, 14) / (count * (count * multi)),
-
-            'prm': Processor._sum(joined_sample, l, l / 2, 2, 14) / (3 * count * multi),
-        }
+                    result.values[i][j] += s.values[i][j]
+        return result
 
     @staticmethod
     def _sum(sample, t, b, l, r):
+        """
+        Sum over two dim space.
+        :param t: top (not inclusive)
+        :param b: bottom (inclusive)
+        :param l: left (inclusive)
+        :param r: right (not inclusive)
+        """
         total = 0.
         for i in range(b, t):
             for j in range(l, r):
